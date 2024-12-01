5f4a73c55f5e81f799f17a24ffdf3df50c297d51
Carl Bennett <carl@carlbennett.me>
2024-12-01 17:36:03 -0600

    Add jsonSerialize to Core/PhpInfo model

--- src/Models/Core/PhpInfo.php
+++ src/Models/Core/PhpInfo.php
@@ -2,7 +2,12 @@
 
 namespace BNETDocs\Models\Core;
 
-class PhpInfo extends \BNETDocs\Models\ActiveUser
+class PhpInfo extends \BNETDocs\Models\ActiveUser implements \JsonSerializable
 {
     public ?string $phpinfo = null;
+
+    public function jsonSerialize(): mixed
+    {
+        return \array_merge(parent::jsonSerialize(), ['phpinfo' => $this->phpinfo]);
+    }
 }
