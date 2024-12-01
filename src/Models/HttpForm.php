785b718fc71be77f24494951cdf054cc26602d8e
Carl Bennett <carl@carlbennett.me>
2024-11-30 22:25:49 -0600

    Add indentation

--- src/Models/HttpForm.php
+++ src/Models/HttpForm.php
@@ -4,15 +4,18 @@ namespace BNETDocs\Models;
 
 class HttpForm extends ActiveUser implements \JsonSerializable
 {
-  /**
-   * The key-value store of the form.
-   *
-   * @var array
-   */
-  public array $form = [];
+    /**
+     * The key-value store of the form.
+     *
+     * @var array
+     */
+    public array $form = [];
 
-  public function jsonSerialize(): mixed
-  {
-    return \array_merge(parent::jsonSerialize(), ['form' => $this->form]);
-  }
+    /**
+     * Implements the JSON serialization function from the JsonSerializable interface.
+     */
+    public function jsonSerialize(): mixed
+    {
+        return \array_merge(parent::jsonSerialize(), ['form' => $this->form]);
+    }
 }
