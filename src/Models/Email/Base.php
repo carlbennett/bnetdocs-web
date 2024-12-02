75561b780c22b14e254c271640ead5003cc09ec2
Carl Bennett <carl@carlbennett.me>
2024-12-01 18:24:41 -0600

    Remove unused Email models

--- src/Models/Email/Base.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace BNETDocs\Models\Email;
-
-abstract class Base implements \BNETDocs\Interfaces\Model
-{
-    public ?\BNETDocs\Libraries\User\User $active_user;
-    public $mail;
-}
