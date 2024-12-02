5d08776a23f0aaf39726cdb1f7030560d195f804
Carl Bennett <carl@carlbennett.me>
2024-12-01 19:22:32 -0600

    Add jsonSerialize to EventLog models

--- src/Models/EventLog/Index.php
+++ src/Models/EventLog/Index.php
@@ -2,13 +2,26 @@
 
 namespace BNETDocs\Models\EventLog;
 
-class Index extends \BNETDocs\Models\ActiveUser
+class Index extends \BNETDocs\Models\ActiveUser implements \JsonSerializable
 {
-  public bool $acl_allowed = false;
-  public ?array $events = null;
-  public int $limit = 0;
-  public string $order = '';
-  public int $page = 0;
-  public int $pages = 0;
-  public int $sum_events = 0;
+    public bool $acl_allowed = false;
+    public ?array $events = null;
+    public int $limit = 0;
+    public string $order = '';
+    public int $page = 0;
+    public int $pages = 0;
+    public int $sum_events = 0;
+
+    public function jsonSerialize(): mixed
+    {
+        return \array_merge(parent::jsonSerialize(), [
+            'acl_allowed' => $this->acl_allowed,
+            'events' => $this->events,
+            'limit' => $this->limit,
+            'order' => $this->order,
+            'page' => $this->page,
+            'pages' => $this->pages,
+            'sum_events' => $this->sum_events,
+        ]);
+    }
 }
--- src/Models/EventLog/View.php
+++ src/Models/EventLog/View.php
@@ -2,9 +2,18 @@
 
 namespace BNETDocs\Models\EventLog;
 
-class View extends \BNETDocs\Models\ActiveUser
+class View extends \BNETDocs\Models\ActiveUser implements \JsonSerializable
 {
-  public bool $acl_allowed = false;
-  public ?\BNETDocs\Libraries\EventLog\Event $event = null;
-  public ?int $id = null;
+    public bool $acl_allowed = false;
+    public ?\BNETDocs\Libraries\EventLog\Event $event = null;
+    public ?int $id = null;
+
+    public function jsonSerialize(): mixed
+    {
+        return \array_merge(parent::jsonSerialize(), [
+            'acl_allowed' => $this->acl_allowed,
+            'event' => $this->event,
+            'id' => $this->id,
+        ]);
+    }
 }
